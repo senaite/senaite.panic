0bfd18e585abc8dad17f9002c37e0036b22e7e33
Ramon Bartl <rb@ridingbytes.com>
2020-06-09 14:23:58 +0200

    Fixed email popup JS

--- src/senaite/panic/static/js/email_popup.js
+++ src/senaite/panic/static/js/email_popup.js
@@ -1,27 +1,27 @@
 (function( $ ) {
-$(document).ready(function(){
+  $(document).ready(function(){
 
-if ($('#panic_email_popup').length) {
-    $("#panic_email_popup").click(function(event){
+    if ($('#panic_email_popup').length) {
+      $("#panic_email_popup").click(function(event){
         event.preventDefault();
         var dialog = $('<div></div>');
         dialog
-            .load(window.portal_url + "/panic_email_popup",
+          .load(window.portal_url + "/panic_email_popup",
                 {'uid':$('input[name="email_popup_uid"]').val(),
                  '_authenticator': $('input[name="_authenticator"]').val()}
-            )
-            .dialog({
-                width:700,
-                height:600,
-                closeText: _("Close"),
-                resizable:true,
-                title: $(this).text()
-            });
-    });
-    if ($('input[name="email_popup_uid"]').attr('autoshow')=='True') {
+               )
+          .dialog({
+            width:700,
+            height:600,
+            closeText: _t("Close"),
+            resizable:true,
+            title: $(this).text()
+          });
+      });
+      if ($('input[name="email_popup_uid"]').attr('autoshow')=='True') {
         $('#panic_email_popup').click();
+      }
     }
-}
 
-});
+  });
 }(jQuery));
